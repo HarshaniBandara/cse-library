4296a8149c719029532148b866094d99fb7fdc72
poornaSavindi <prnsavindi11@gmail.com>
2021-09-23 12:08:50 +0530

    Revert "created sum of primes"
    This reverts commit c6f41ebcf31aa90c227aa34cd24df8f582223d16.

--- src/main/java/it/uom/cse/MathOperation.java
+++ src/main/java/it/uom/cse/MathOperation.java
@@ -15,26 +15,4 @@ public class MathOperation {
         return operand1 - operand2;
     }
 
-    public static int sumOfPrimes(int n){
-        int sum = 0;
-        for (int i = n; i >= 0; i--) {
-            if (isPrime(i)) {
-                sum = sum + i;
-            }
-        }
-        return sum;
-    }
-
-    private static boolean isPrime(int n){
-        if(n == 1) return false;
-        for (int i = 2; i*i <= n; i++)
-        {
-            if (n % i == 0)
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-
 }
